1ca31ea6444cfb951a8cfb255895752818b54e4b
tslearn <tslearn@163.com>
2025-04-22 01:06:07 +0800

    change

--- log.go
+++ log.go
@@ -6,107 +6,107 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// Trace logs a message at level Trace on the standard logger.
-func Trace(args ...any) {
+// LogTrace logs a message at level Trace on the standard logger.
+func LogTrace(args ...any) {
 	log.Trace(args...)
 }
 
-// Debug logs a message at level Debug on the standard logger.
-func Debug(args ...any) {
+// LogDebug logs a message at level Debug on the standard logger.
+func LogDebug(args ...any) {
 	log.Debug(args...)
 }
 
-// Info logs a message at level Info on the standard logger.
-func Info(args ...any) {
+// LogInfo logs a message at level Info on the standard logger.
+func LogInfo(args ...any) {
 	log.Info(args...)
 }
 
-// Warn logs a message at level Warn on the standard logger.
-func Warn(args ...any) {
+// LogWarn logs a message at level Warn on the standard logger.
+func LogWarn(args ...any) {
 	log.Warn(args...)
 }
 
-// Error logs a message at level Error on the standard logger.
-func Error(args ...any) {
+// LogError logs a message at level Error on the standard logger.
+func LogError(args ...any) {
 	log.Error(args...)
 }
 
-// Panic logs a message at level Panic on the standard logger.
-func Panic(args ...any) {
+// LogPanic logs a message at level Panic on the standard logger.
+func LogPanic(args ...any) {
 	log.Panic(args...)
 }
 
-// Fatal logs a message at level Fatal on the standard logger then the process will exit with status set to 1.
-func Fatal(args ...any) {
+// LogFatal logs a message at level Fatal on the standard logger then the process will exit with status set to 1.
+func LogFatal(args ...any) {
 	log.Fatal(args...)
 }
 
-// Tracef logs a message at level Trace on the standard logger.
-func Tracef(format string, args ...any) {
+// LogTracef logs a message at level Trace on the standard logger.
+func LogTracef(format string, args ...any) {
 	log.Tracef(format, args...)
 }
 
-// Debugf logs a message at level Debug on the standard logger.
-func Debugf(format string, args ...any) {
+// LogDebugf logs a message at level Debug on the standard logger.
+func LogDebugf(format string, args ...any) {
 	log.Debugf(format, args...)
 }
 
-// Infof logs a message at level Info on the standard logger.
-func Infof(format string, args ...any) {
+// LogInfof logs a message at level Info on the standard logger.
+func LogInfof(format string, args ...any) {
 	log.Infof(format, args...)
 }
 
-// Warnf logs a message at level Warn on the standard logger.
-func Warnf(format string, args ...any) {
+// LogWarnf logs a message at level Warn on the standard logger.
+func LogWarnf(format string, args ...any) {
 	log.Warnf(format, args...)
 }
 
-// Errorf logs a message at level Error on the standard logger.
-func Errorf(format string, args ...any) {
+// LogErrorf logs a message at level Error on the standard logger.
+func LogErrorf(format string, args ...any) {
 	log.Errorf(format, args...)
 }
 
-// Panicf logs a message at level Panic on the standard logger.
-func Panicf(format string, args ...any) {
+// LogPanicf logs a message at level Panic on the standard logger.
+func LogPanicf(format string, args ...any) {
 	log.Panicf(format, args...)
 }
 
-// Fatalf logs a message at level Fatal on the standard logger then the process will exit with status set to 1.
-func Fatalf(format string, args ...any) {
+// LogFatalf logs a message at level Fatal on the standard logger then the process will exit with status set to 1.
+func LogFatalf(format string, args ...any) {
 	log.Fatalf(format, args...)
 }
 
-// Traceln logs a message at level Trace on the standard logger.
-func Traceln(args ...any) {
+// LogTraceln logs a message at level Trace on the standard logger.
+func LogTraceln(args ...any) {
 	log.Traceln(args...)
 }
 
-// Debugln logs a message at level Debug on the standard logger.
-func Debugln(args ...any) {
+// LogDebugln logs a message at level Debug on the standard logger.
+func LogDebugln(args ...any) {
 	log.Debugln(args...)
 }
 
-// Infoln logs a message at level Info on the standard logger.
-func Infoln(args ...any) {
+// LogInfoln logs a message at level Info on the standard logger.
+func LogInfoln(args ...any) {
 	log.Infoln(args...)
 }
 
-// Warnln logs a message at level Warn on the standard logger.
-func Warnln(args ...any) {
+// LogWarnln logs a message at level Warn on the standard logger.
+func LogWarnln(args ...any) {
 	log.Warnln(args...)
 }
 
-// Errorln logs a message at level Error on the standard logger.
-func Errorln(args ...any) {
+// LogErrorln logs a message at level Error on the standard logger.
+func LogErrorln(args ...any) {
 	log.Errorln(args...)
 }
 
-// Panicln logs a message at level Panic on the standard logger.
-func Panicln(args ...any) {
+// LogPanicln logs a message at level Panic on the standard logger.
+func LogPanicln(args ...any) {
 	log.Panicln(args...)
 }
 
-// Fatalln logs a message at level Fatal on the standard logger then the process will exit with status set to 1.
-func Fatalln(args ...any) {
+// LogFatalln logs a message at level Fatal on the standard logger then the process will exit with status set to 1.
+func LogFatalln(args ...any) {
 	log.Fatalln(args...)
 }
