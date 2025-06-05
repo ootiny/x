9622ff1af942876848b62004833db30f140c756e
tslearn <tslearn@163.com>
2025-06-06 06:33:26 +0800

    change

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/ootiny/x"
 )
 
-func main() {
-	opt := x.NewSSHOptionWithPassword("tianshuo", "192.168.1.81", "World2019")
+func RestoreEsxiVM(vmid int, snapshotId int) error {
+	opt := x.NewSSHOptionWithPassword("root", "192.168.1.11", "Tscc0805@")
 	opt.Expect = func(output string) (string, error) {
-		if strings.Contains(output, "[sudo] password for") {
-			return "World2019\n", nil
+		fmt.Println(output)
+		if strings.Contains(output, "Tscc0805@") {
+			return "Tscc0805@\n", nil
 		}
-
 		return "", nil
 	}
-
-	// if _, err := x.SSH("sudo -S apt-get update", opt); err != nil {
-	// 	panic(err)
-	// }
-
-	if err := x.SCP("/Users/tianshuo/Downloads/debian-12.11.0-arm64-DVD-1.iso", "/home/tianshuo/1.txt", opt); err != nil {
-		panic(err)
+	if _, err := x.SSH(fmt.Sprintf("vim-cmd vmsvc/snapshot.revert %d %d 0", vmid, snapshotId), opt); err != nil {
+		return err
+	} else {
+		return nil
 	}
+}
+
+func main() {
+	fmt.Println(RestoreEsxiVM(56, 1))
 }
