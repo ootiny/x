8deaf780cf5ffe0effdb3523e545ccbffae3818b
tslearn <tslearn@163.com>
2025-06-16 08:37:17 +0000

    change

--- common.go
+++ common.go
@@ -1,1 +1,62 @@
 package x
+
+import (
+	"embed"
+	"net"
+	"strings"
+	"time"
+)
+
+func Ternary[T any](cond bool, trueValue, falseValue T) T {
+	if cond {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
+func WaitForTCP(network string, ip string, port uint16, timeout time.Duration) bool {
+	deadLine := time.Now().Add(timeout)
+
+	for time.Now().Before(deadLine) {
+		ColorPrintf("blue", "Waiting for %s %s:%d", network, ip, port)
+		conn, err := net.DialTimeout(network, Sprintf("%s:%d", ip, port), time.Second*3)
+		if err != nil {
+			ColorPrintf("blue", " ...\n")
+			time.Sleep(time.Second)
+			continue
+		} else {
+			ColorPrintf("green", " OK\n")
+			conn.Close()
+			return true
+		}
+	}
+
+	return false
+}
+
+func fnListJsonTasks(efs embed.FS, dir string, subDir string) ([]string, error) {
+	var result []string
+
+	if entries, err := efs.ReadDir(dir); err != nil {
+		return nil, err
+	} else {
+		for _, entry := range entries {
+			if entry.IsDir() {
+				if subFiles, err := fnListJsonTasks(efs, dir, Ternary(subDir == "", entry.Name(), subDir+"/"+entry.Name())); err != nil {
+					return nil, err
+				} else {
+					result = append(result, subFiles...)
+				}
+			} else if strings.HasSuffix(entry.Name(), ".json") {
+				result = append(result, subDir+"/"+entry.Name())
+			}
+		}
+
+		return result, nil
+	}
+}
+
+func ListJsonTasks(efs embed.FS, dir string) ([]string, error) {
+	return fnListJsonTasks(efs, dir, "")
+}
