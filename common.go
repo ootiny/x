1b3be11b5fc64a989c514370d45edfff18b11335
tslearn <tslearn@163.com>
2025-06-16 08:51:54 +0000

    change

--- common.go
+++ common.go
@@ -7,14 +7,6 @@ import (
 	"time"
 )
 
-func Ternary[T any](cond bool, trueValue, falseValue T) T {
-	if cond {
-		return trueValue
-	} else {
-		return falseValue
-	}
-}
-
 func WaitForTCP(network string, ip string, port uint16, timeout time.Duration) bool {
 	deadLine := time.Now().Add(timeout)
 
@@ -35,6 +27,14 @@ func WaitForTCP(network string, ip string, port uint16, timeout time.Duration) b
 	return false
 }
 
+func Ternary[T any](cond bool, trueValue, falseValue T) T {
+	if cond {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
 func fnListJsonTasks(efs embed.FS, dir string, subDir string) ([]string, error) {
 	var result []string
 
@@ -42,14 +42,15 @@ func fnListJsonTasks(efs embed.FS, dir string, subDir string) ([]string, error) 
 		return nil, err
 	} else {
 		for _, entry := range entries {
+			subPath := Ternary(subDir == "", entry.Name(), subDir+"/"+entry.Name())
 			if entry.IsDir() {
-				if subFiles, err := fnListJsonTasks(efs, dir, Ternary(subDir == "", entry.Name(), subDir+"/"+entry.Name())); err != nil {
+				if subFiles, err := fnListJsonTasks(efs, dir, subPath); err != nil {
 					return nil, err
 				} else {
 					result = append(result, subFiles...)
 				}
 			} else if strings.HasSuffix(entry.Name(), ".json") {
-				result = append(result, subDir+"/"+entry.Name())
+				result = append(result, strings.TrimSuffix(subPath, ".json"))
 			}
 		}
 
