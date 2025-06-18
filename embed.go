4a0dd9bf42d1f0581af7d49c777c26f1426b07a3
tslearn <tslearn@163.com>
2025-06-18 10:06:13 +0000

    change

--- embed.go
+++ embed.go
@@ -2,8 +2,15 @@ package x
 
 import (
 	"embed"
+	"encoding/json"
+	"maps"
 	"strings"
 )
+
+func IsFileExists(efs embed.FS, path string) bool {
+	_, err := efs.ReadFile(path)
+	return err == nil
+}
 
 func fnListJsonTasks(efs embed.FS, dir string, subDir string) ([]string, error) {
 	var result []string
@@ -30,4 +37,35 @@ func fnListJsonTasks(efs embed.FS, dir string, subDir string) ([]string, error) 
 
 func ListJsonTasks(efs embed.FS, dir string) ([]string, error) {
 	return fnListJsonTasks(efs, dir, "")
+}
+
+func LoadTaskConfig(
+	efs embed.FS, efsPath string,
+	config any, taskName string, overrideConfig string) error {
+	taskJSON, err := efs.ReadFile(efsPath + taskName + ".json")
+	if err != nil {
+		return err
+	}
+
+	baseConfig := map[string]any{}
+	if err := json.Unmarshal(taskJSON, &baseConfig); err != nil {
+		return Errorf(`task %s: failed to unmarshal base config: %v`, taskName, err)
+	}
+
+	overrideConfigMap := map[string]any{}
+	if overrideConfig != "" {
+		if err := json.Unmarshal([]byte(overrideConfig), &overrideConfigMap); err != nil {
+			return Errorf(`task %s: failed to unmarshal override config: %v`, taskName, err)
+		}
+	}
+
+	maps.Copy(baseConfig, overrideConfigMap)
+
+	if mergedConfig, err := json.Marshal(baseConfig); err != nil {
+		return Errorf(`task %s: failed to marshal merged config: %v`, taskName, err)
+	} else if err := json.Unmarshal(mergedConfig, config); err != nil {
+		return Errorf(`task %s: failed to unmarshal merged config: %v`, taskName, err)
+	} else {
+		return nil
+	}
 }
